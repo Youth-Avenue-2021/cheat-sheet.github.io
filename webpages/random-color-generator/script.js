a9e02c78358b7ea2aca58608430591208fc94e8b
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2021-04-24 14:09:41 +0530

    add more codes in css file and fix some problems and add some copy functions in random color generator...

--- webpages/random-color-generator/script.js
+++ webpages/random-color-generator/script.js
@@ -5,5 +5,31 @@ function changeColor() {
     let background = `rgb(${parseInt(Math.random() * 255)},${parseInt(Math.random() * 255)},${parseInt(Math.random() * 255)})`;
     body.style.background = background;
     body.style.transition = 'background 0.6s ease';
-    randomColor.innerText = background;
+    randomColor.value = background;
 }
+
+
+function copyTextFunction() {
+  var copyText = document.getElementById("randomColor");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  // alert("Copied the text: " + copyText.innerText);
+  showCopy();
+}
+
+function showCopy(){
+	// document.getElementById("copyTextDiv").style.animationName='show';
+	document.getElementById('copyTextDiv').style.opacity = '1';
+	document.getElementById('copyTextDiv').style.top = '80%';
+	// document.getElementById("randomColor").style.opacity = '1';
+	setTimeout(function hideShowCopy(){
+		document.getElementById('copyTextDiv').style.opacity = '0';
+		document.getElementById('copyTextDiv').style.top = '70%';	
+	},2000);
+}
+
+// function hideShowCopy(){
+// 	document.getElementById('copyTextDiv').style.opacity = '0';
+// 	document.getElementById('copyTextDiv').style.top = '65%';	
+// }
