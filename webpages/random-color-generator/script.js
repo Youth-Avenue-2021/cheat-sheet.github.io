00295a125ff032ca49a28645ba2c164a1206882c
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2021-04-24 15:49:03 +0530

    added color adding features in random color generator...

--- webpages/random-color-generator/script.js
+++ webpages/random-color-generator/script.js
@@ -1,21 +1,35 @@
 let randomColor = document.getElementById('randomColor');
+let addColors=document.getElementById('addColors');
+let arr=[];
+
+var firstNumber;
+var secondNumber;
+var thirdNumber;
 
 function changeColor() {
     let body = document.querySelector('html');
-    let background = `rgb(${parseInt(Math.random() * 255)},${parseInt(Math.random() * 255)},${parseInt(Math.random() * 255)})`;
-    body.style.background = background;
-    body.style.transition = 'background 0.6s ease';
+    firstNumber=parseInt(Math.random() * 255);
+    secondNumber=parseInt(Math.random() * 255);
+    thirdNumber=parseInt(Math.random() * 255);
+    let background = `rgb(${firstNumber},${secondNumber},${thirdNumber})`;
+    document.getElementById('first-section-color').style.background = background;
+    document.getElementById('first-section-color').style.transition = 'background 0.6s ease';
     randomColor.value = background;
 }
 
 
+
+
 function copyTextFunction() {
-  var copyText = document.getElementById("randomColor");
-  // copyText.select();
-  // copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  // alert("Copied the text: " + copyText.innerText);
-  showCopy();
+	var copyText = document.getElementById("randomColor");
+	copyText.select();
+	// copyText.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+	// alert("Copied the text: " + copyText.innerText);
+	showCopy();
+	arr.push(copyText.value);
+	console.log(arr);
+	addColorInCode();
 }
 
 function showCopy(){
@@ -29,7 +43,16 @@ function showCopy(){
 	},2000);
 }
 
-// function hideShowCopy(){
-// 	document.getElementById('copyTextDiv').style.opacity = '0';
-// 	document.getElementById('copyTextDiv').style.top = '65%';	
-// }
+
+function addColorInCode(){
+
+// console.log(firstNumber, secondNumber, thirdNumber);
+	addColors.innerHTML+=`
+<span class="token property">color${arr.length}</span><span class="token punctuation">:</span> <span class="token color"><span class="inline-color-wrapper"><span class="inline-color" style="background-color:${arr[(arr.length)-1]};"></span></span><span class="token function">rgb</span><span class="token punctuation">(</span><span class="token number">${firstNumber}</span><span class="token punctuation">,</span><span class="token number">${secondNumber}</span><span class="token punctuation">,</span><span class="token number">${thirdNumber}</span><span class="token punctuation">)</span></span>;`;
+
+}
+
+/*
+	// addColors.innerHTML+=`color${arr.length}: ${arr[(arr.length)-1]}
+
+	*/
