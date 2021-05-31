3ee10f5c0f27415e954b921c68be77e20a736e2d
Abhi Dadhaniya <abhidadhaniya23@gmail.com>
2021-05-31 23:28:27 +0530

    rename all space included pdf files and added all data and pages for download eg materials...

---
+++ cheat-sheet/eg-script/eg-script.js
@@ -0,0 +1,101 @@
+let filesData = [
+    {
+        'fileName': 'EG Syllabus',
+        'fileData': 'EG_Syllabus.pdf'
+    },
+    {
+        'fileName': 'EG Scales 1',
+        'fileData': 'Engineering_Scales_I.pdf'
+    },
+    {
+        'fileName': 'EG Scales 2',
+        'fileData': 'Engineering_Scales_II.pdf'
+    },
+    {
+        'fileName': 'EG Manual',
+        'fileData': 'EG_MANUAL_2021.pdf'
+    },
+    {
+        'fileName': 'EG Curves',
+        'fileData': 'Engineering_Curves_I.pdf'
+    },
+    {
+        'fileName': 'Orthographic Projection 1',
+        'fileData': 'Orthographic_Projection_I.pdf'
+    },
+    {
+        'fileName': 'Orthographic Projection 2',
+        'fileData': 'Orthographic_Projection_II.pdf'
+    },
+    {
+        'fileName': 'SECTIONAL VIEWS Students (ppt)',
+        'fileData': 'SECTIONAL_VIEWS_Students.ppt'
+    }
+]
+
+const downloadMaterialsDiv = document.querySelector('.download-materials');
+let waitingSeconds = 10;
+
+function addAds() {
+
+    let createAd = document.createElement('div')
+    createAd.setAttribute('class', 'ads-section')
+    createAd.setAttribute('class', 'flex-column')
+    createAd.innerHTML = `
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <!-- long-width -->
+    <ins class="adsbygoogle" style="display:block;width:100%;height:300px;margin:20px auto;"
+    data-ad-client="ca-pub-6036528993346308" data-ad-slot="6037331449" data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    `
+    downloadMaterialsDiv.append(createAd)
+}
+
+function getFileData(fileName, fileData) {
+
+    let createFileData = document.createElement('div')
+    createFileData.setAttribute('class', 'saparate-eg-file flex-column')
+
+    let createfileDesc = document.createElement('p')
+    createfileDesc.setAttribute('class', 'file-desc')
+    createfileDesc.innerText = fileName
+    createFileData.append(createfileDesc)
+
+    let createfileDownload = document.createElement('a')
+    createfileDownload.setAttribute('class', 'eg-download-btn')
+    createfileDownload.setAttribute('href', `../pdf/EG-Materials/${fileData}`)
+    createfileDownload.innerText = 'Download'
+    createFileData.append(createfileDownload)
+
+    downloadMaterialsDiv.append(createFileData);
+
+}
+const progressBarTimer = document.getElementById('progress_bar_timer');
+const egWaitingNote = document.getElementById('eg-waiting-note');
+const waitingTimerDiv=document.querySelector('.waiting-timer-div');
+
+let i = 0;
+
+let timerOfInterval = setInterval(() => {
+    progressBarTimer.innerText = waitingSeconds
+    egWaitingNote.innerText = `Please wait ${waitingSeconds} seconds for download all EG-Materials...`
+    if(waitingSeconds==-1){
+        waitingTimerDiv.style.display='none';
+        document.getElementById('topAd').style.display='none';
+        clearInterval(timerOfInterval);
+    }
+    waitingSeconds--
+}, 1000);
+
+setTimeout(() => {
+    filesData.forEach(file => {
+        getFileData(file.fileName, file.fileData)
+        if (i % 2 == 0) {
+            addAds()
+        }
+        i++
+    })
+}, (waitingSeconds+2) * 1000);
